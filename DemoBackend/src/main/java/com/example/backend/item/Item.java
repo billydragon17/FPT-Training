38168d34febdf7d2f4c93f7502eb2e1ae4240ab9
LongNguyen17 <l.nguyen@student.xu-university.de>
2022-03-21 14:38:26 +0700

    Created Setters

--- DemoBackend/src/main/java/com/example/backend/item/Item.java
+++ DemoBackend/src/main/java/com/example/backend/item/Item.java
@@ -13,27 +13,27 @@ import javax.validation.constraints.*;
 @Entity
 public class Item {
     @Id
-    private final Long id;
+    private  Long id;
 
     @NotNull(message = "name is required")
     @Pattern(regexp="^[a-zA-Z ]+$", message = "name must be a string")
     // @Column(value = "student_name")
-    private final String name;
+    private String name;
 
     @NotNull(message = "age is required")
     @Positive(message = "age must be positive")
     // @Column(value = "age")
-    private final Long age;
+    private Long age;
 
     @NotNull(message = "description is required")
     @Pattern(regexp="^[a-zA-Z ]+$", message = "description must be a string")
     // @Column(value = "student_description")
-    private final String description;
+    private  String description;
 
     @NotNull(message = "image is required")
     @URL(message = "image must be a URL")
     // @Column(value = "image")
-    private final String image;
+    private  String image;
 
     public Item(
             Long id,
@@ -69,6 +69,34 @@ public class Item {
     public String getImage() {
         return image;
     }
+
+
+    public Long setId(Long id)
+    {
+        return this.id = id;
+    }
+
+    public Long setAge(Long age)
+    {
+        return this.age = age;
+    }
+
+    public String setName(String name)
+    {
+        return this.name = name;
+    }
+
+    public String setDescription(String description)
+    {
+        return this.description = description;
+    }
+
+    public String setImage(String image)
+    {
+        return this.image = image;
+    }
+
+    
 
     public Item updateWith(Item item) {
         return new Item(
