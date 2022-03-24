dc4edbd5dd87f7fc735bf2e2ddd5b0b7758fe80f
LongNguyen17 <l.nguyen@student.xu-university.de>
2022-03-24 16:16:22 +0700

    Commented out default items in db

--- DemoBackend/src/main/java/com/example/backend/item/ItemService.java
+++ DemoBackend/src/main/java/com/example/backend/item/ItemService.java
@@ -17,16 +17,19 @@ public class ItemService {
 
     public ItemService(CrudRepository<Item, Long> repository) {
         this.repository = repository;
-        this.repository.saveAll(defaultItems());
+        //this.repository.saveAll(defaultItems());
     }
 
-    private static List<Item> defaultItems() {
-        return List.of(
-                new Item(1L, "Burger", 599L, "Tasty", "https://cdn.auth0.com/blog/whatabyte/burger-sm.png"),
-                new Item(2L, "Pizza", 299L, "Delicious", "https://cdn.auth0.com/blog/whatabyte/pizza-sm.png"),
-                new Item(3L, "Tea", 199L, "Informative", "https://cdn.auth0.com/blog/whatabyte/tea-sm.png")
-        );
-    }
+
+    /* */
+    
+    // private static List<Item> defaultItems() {
+    //     return List.of(
+    //             new Item(1L, "Burger", 599L, "Tasty", "https://cdn.auth0.com/blog/whatabyte/burger-sm.png"),
+    //             new Item(2L, "Pizza", 299L, "Delicious", "https://cdn.auth0.com/blog/whatabyte/pizza-sm.png"),
+    //             new Item(3L, "Tea", 199L, "Informative", "https://cdn.auth0.com/blog/whatabyte/tea-sm.png")
+    //     );
+    // }
 
     public List<Item> findAll() {
         List<Item> list = new ArrayList<>();
