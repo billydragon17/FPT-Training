fdf1d480cef33864e1fe5d02bdffd0b0eeb45b77
LongNguyen17 <l.nguyen@student.xu-university.de>
2022-04-03 15:06:15 +0200

    Routing update

--- DemoApp/src/app/app-routing.module.ts
+++ DemoApp/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StudentListComponent } from './student-list/student-list.component';
+import { StudentFormComponent } from './student-form/student-form.component';
 
 const routes: Routes = [
-  { path: 'students', component: StudentListComponent}
+  { path: 'students', component: StudentListComponent},
+  { path: 'addstudent', component: StudentFormComponent}
 ];
 
 @NgModule({
