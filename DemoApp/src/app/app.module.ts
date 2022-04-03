859097b1ba52a37087e0cbf4048ba1c3eb3cbd39
LongNguyen17 <l.nguyen@student.xu-university.de>
2022-04-03 15:07:55 +0200

    Update app.module.ts

--- DemoApp/src/app/app.module.ts
+++ DemoApp/src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentListComponent } from './student-list/student-list.component';
+import { StudentFormComponent } from './student-form/student-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentListComponent,
-    
+    StudentFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
